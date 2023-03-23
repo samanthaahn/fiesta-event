c4ec182ff75ca2111282561008d1ec689c73a6b1
shawn220a <shawn.42496@gmail.com>
2023-03-23 14:58:26 -0500

    Refactor code

--- assets/script.js
+++ assets/script.js
@@ -22,23 +22,23 @@
 //inauthor: Returns results where the text following this keyword is found in the author
 
 //function for the button
-var searchformEl = document.querySelector("#search-form");
-var genreFormEl = document.querySelector("#format-input");
+var searchformEl = document.querySelector('#search-form');
+var genreFormEl = document.querySelector('#format-input');
 var resultscontentEl = document.querySelector('.resultcontent');
 function handleSearchFormSubmit(event) {
   event.preventDefault();
 
-  var searchInputVal = document.querySelector("#search-input").value;
+  var searchInputVal = document.querySelector('#search-input').value;
   // var formatInputVal = document.querySelector('#format-input').value;
 
   if (!searchInputVal && !genreFormEl.value) {
-    console.error("You need input something to search up or click a genre!");
+    console.error('You need input something to search up or click a genre!');
     return;
   }
 
   if (searchInputVal) {
     var queryString =
-      "https://www.googleapis.com/books/v1/volumes?q=inauthor:" +
+      'https://www.googleapis.com/books/v1/volumes?q=inauthor:' +
       searchInputVal;
 
     fetch(queryString)
@@ -46,86 +46,62 @@ function handleSearchFormSubmit(event) {
         return response.json();
       })
       .then(function (data) {
-        console.log(data);
+        var { items } = data;
 
-        //add create element, textContent, append
-
-        console.log(data);
-
-        // set up `<div>` to hold result content
-        // var resultauthors = document.createElement('div');
-        // resultauthors.classList.add('card', 'col-6');
-
-        var resultCard = document.createElement('div');
-        // resultCard.classList.add('card', 'bg-light', 'text-dark', 'mb-3', 'p-3');
-
-        var resultBody = document.createElement('div');
-        // resultBody.classList.add('card-body');
-        resultCard.append(resultBody);
-
-        var titleEl = document.createElement('h3');
-        titleEl.textContent = data.items[0].volumeInfo.title;
-
-
-
-        var bodyContentEl = document.createElement('p');
-        bodyContentEl.innerHTML =
-          '<strong>Author:</strong> ' + data.items[0].volumeInfo.authors + '<br/>';
-        var imageEl = document.createElement('img');
-        imageEl.innerHTML =
-          '<strong>Image:</strong>' + data.items[0].volumeInfo.imageLinks.smallThumbnail + '<br/>';
-
-        resultBody.append(bodyContentEl, imageEl, titleEl)
-        resultscontentEl.append(resultCard)
-
-        if (data.items) {
-          bodyContentEl.innerHTML +=
-            '<strong>Title:</strong> ' + data.items[0].volumeInfo.title + '<br/>';
-        } else {
-          bodyContentEl.innerHTML +=
-            '<strong>Title:</strong> No description for this entry.';
+        for (var i = 0; i < items.length; i++) {
+          var { volumeInfo } = items[i];
+          printResults(volumeInfo);
         }
-
-        if (data.items) {
-          bodyContentEl.innerHTML +=
-            '<strong>Image:</strong> ' + data.items[0].volumeInfo.imageLinks.smallThumbnail + '<br/>';
-        } else {
-          bodyContentEl.innerHTML +=
-            '<strong>Image:</strong> No description for this entry.';
-        }
-
-        if (data.items) {
-          bodyContentEl.innerHTML +=
-            '<strong>Description:</strong> ' + data.description[0];
-        } else {
-          bodyContentEl.innerHTML +=
-            '<strong>Description:</strong>  No description for this entry.';
-        }
-
-        // for (var i = 0; i < data.items.length; i++) {
-        //   printResults(data.items[i]);
-        // }
-      })
-}}
-
-  function handleGenreSubmit() {
-    var queryFormat =
-      "https://www.googleapis.com/books/v1/volumes?q=subject:" +
-      genreFormEl.value;
-
-    fetch(queryFormat)
-      .then(function (response) {
-        return response.json();
-      })
-      .then(function (data) {
-        console.log(data);
-
-        //add create element, textContent, append
       });
   }
-  if (genreFormEl.value) {
-    handleGenreSubmit();
-  }
+}
 
+function printResults(authorList) {
+  console.log(authorList);
 
-  searchformEl.addEventListener("submit", handleSearchFormSubmit); 
+  //add create element, textContent, append
+
+  // set up `<div>` to hold result content
+  // var resultauthors = document.createElement('div');
+  // resultauthors.classList.add('card', 'col-6');
+
+  var resultCard = document.createElement('div');
+  // resultCard.classList.add('card', 'bg-light', 'text-dark', 'mb-3', 'p-3');
+
+  var resultBody = document.createElement('div');
+  // resultBody.classList.add('card-body');
+  resultCard.append(resultBody);
+
+  var titleEl = document.createElement('h3');
+  titleEl.textContent = authorList.title;
+
+  var bodyContentEl = document.createElement('p');
+  bodyContentEl.innerHTML =
+    '<strong>Author:</strong> ' + authorList.authors + '<br/>';
+  var imageEl = document.createElement('img');
+  imageEl.src = authorList.imageLinks.smallThumbnail;
+
+  resultBody.append(titleEl, bodyContentEl, imageEl);
+  resultscontentEl.append(resultCard);
+}
+
+function handleGenreSubmit() {
+  var queryFormat =
+    'https://www.googleapis.com/books/v1/volumes?q=subject:' +
+    genreFormEl.value;
+
+  fetch(queryFormat)
+    .then(function (response) {
+      return response.json();
+    })
+    .then(function (data) {
+      console.log(data);
+
+      //add create element, textContent, append
+    });
+}
+if (genreFormEl.value) {
+  handleGenreSubmit();
+}
+
+searchformEl.addEventListener('submit', handleSearchFormSubmit);
