1b8dd3af3a00efcab782d7652c2936c1c617aa0b
Samantha Ahn <samanthaahn@Samanthas-MBP.attlocal.net>
2023-03-23 11:07:04 -0700

    added some parameters for the search in js

--- assets/script.js
+++ assets/script.js
@@ -21,74 +21,93 @@
 //intitle:returns results where the text following this keyword is found in the title
 //inauthor: Returns results where the text following this keyword is found in the author
 
-
-
 //function for the button
-var searchformEl = document.querySelector('#search-form');
-var genreFormEl = document.querySelector('#format-input');
+var searchformEl = document.querySelector("#search-form");
+var genreFormEl = document.querySelector("#format-input");
 
 function handleSearchFormSubmit(event) {
-    event.preventDefault();
+  event.preventDefault();
 
-    var searchInputVal = document.querySelector('#search-input').value;
-    // var formatInputVal = document.querySelector('#format-input').value;
+  var searchInputVal = document.querySelector("#search-input").value;
+  // var formatInputVal = document.querySelector('#format-input').value;
 
-    if (!searchInputVal && !genreFormEl.value) {
-        console.error('You need input something to search up or click a genre!');
-        return;
-    }
+  if (!searchInputVal && !genreFormEl.value) {
+    console.error("You need input something to search up or click a genre!");
+    return;
+  }
 
-if (searchInputVal ) {
-    var queryString = 'https://www.googleapis.com/books/v1/volumes?q=inauthor:' + searchInputVal
+  if (searchInputVal) {
+    var queryString =
+      "https://www.googleapis.com/books/v1/volumes?q=inauthor:" +
+      searchInputVal;
 
     fetch(queryString)
-   .then(function(response){
-    return response.json()
-   })
-   .then(function(data){
-    console.log(data)
-   
+      .then(function (response) {
+        return response.json();
+      })
+      .then(function (data) {
+        console.log(data);
 
-//add create element, textContent, append
+        //add create element, textContent, append
+        
+        function printResults(resultObj) {
+            console.log(resultObj);
+          
+            // set up `<div>` to hold result content
 
+            var resultCard = document.createElement('div');
+            resultCard.classList.add('card', 'bg-light', 'text-dark', 'mb-3', 'p-3');
+          
+            var resultBody = document.createElement('div');
+            resultBody.classList.add('card-body');
+            resultCard.append(resultBody);
+          
+            var titleEl = document.createElement('h3');
+            titleEl.textContent = resultObj.title;
 
+            
+          
+            var bodyContentEl = document.createElement('p');
+            bodyContentEl.innerHTML =
+              '<strong>Author:</strong> ' + resultObj.volumeInfo.authors + '<br/>';
+          
+            if (resultObj.title) {
+              bodyContentEl.innerHTML +=
+                '<strong>Title:</strong> ' + resultObj.volumneInfo.title + '<br/>';
+            } else {
+              bodyContentEl.imageLinks +=
+                '<strong>Image:</strong>' + resultObj.volumneInfo.imageLinks.smallThumbnail + '<br/>';
+            }
+          
+            if (resultObj.description) {
+              bodyContentEl.innerHTML +=
+                '<strong>Description:</strong> ' + resultObj.description[0];
+            } else {
+              bodyContentEl.innerHTML +=
+                '<strong>Description:</strong>  No description for this entry.';
+            }
 
-})
+  }});
+  }
 
-}
-
-
-
-   function handleGenreSubmit() {
-    var queryFormat = 'https://www.googleapis.com/books/v1/volumes?q=subject:' + genreFormEl.value
+  function handleGenreSubmit() {
+    var queryFormat =
+      "https://www.googleapis.com/books/v1/volumes?q=subject:" +
+      genreFormEl.value;
 
     fetch(queryFormat)
-    .then(function(response){
-        return response.json()
-    }) 
-    .then(function(data){
-        console.log(data)
-    
-    
-    
-    //add create element, textContent, append
-    
-    
-    
-    
-    })
+      .then(function (response) {
+        return response.json();
+      })
+      .then(function (data) {
+        console.log(data);
 
-
-}
-if (genreFormEl.value) {
+        //add create element, textContent, append
+      });
+  }
+  if (genreFormEl.value) {
     handleGenreSubmit();
+  }
 }
 
-
-}   
-
-
-
-
-
-searchformEl.addEventListener('submit', handleSearchFormSubmit);
+searchformEl.addEventListener("submit", handleSearchFormSubmit);
