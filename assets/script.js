c6b5d1e576bb02aeef18d4b93c2f75e5cf7750d0
Samantha Ahn <samanthaahn@Samanthas-MBP.attlocal.net>
2023-03-24 12:05:14 -0700

    added movie api

--- assets/script.js
+++ assets/script.js
@@ -1,25 +1,15 @@
-// OMDb API: http://www.omdbapi.com/?i=tt3896198&apikey=f5efd3a2
-//API for movie/show info
-// const fetch = require('node-fetch');
+function findmovie(moviename) {
 
-// const url = 'https://streaming-availability.p.rapidapi.com/v2/services';
-// //API for streaming service
-// const options = {
-//     method: 'GET',
-//     headers: {
-//         'X-RapidAPI-Key': '3f0225be7bmshce5c6a43713c859p14d2adjsn50ce1176c7e8',
-//         'X-RapidAPI-Host': 'streaming-availability.p.rapidapi.com'
-//     }
-// };
-
-// fetch(url, options)
-//     .then(res => res.json())
-//     .then(json => console.log(json))
-//     .catch(err => console.error('error:' + err));
-
-//https://www.googleapis.com/books/v1/volumes?q=search+terms
-//intitle:returns results where the text following this keyword is found in the title
-//inauthor: Returns results where the text following this keyword is found in the author
+  var queryMovie = 'https://www.omdbapi.com/?apikey=ea0f7fcf&t=' + moviename
+  fetch(queryMovie)
+  .then(function(response) {
+      return response.json();
+  })
+  .then(function(data) {
+      console.log(data);
+  })
+  }
+  
 
 //function for the button
 var searchformEl = document.querySelector('#search-form');
@@ -48,10 +38,10 @@ function handleSearchFormSubmit(event) {
         for (var i = 0; i < items.length; i++) {
           var { volumeInfo } = items[i];
           printResults(volumeInfo);
+          findmovie(volumeInfo.title);
         }
 
-      }
-      )
+      });
   }
 }
 
